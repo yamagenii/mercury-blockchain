50fedf2f8ea65f8a9088720281a9bf505fc26d43
yamagenii <bass.g.l42@gmail.com>
2017-12-15 19:18:28 +0900

    [add]heroku

--- app.py
+++ app.py
@@ -303,5 +303,5 @@ def index():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    #app.run(host='0.0.0.0', port=port, threaded=True)
-    app.run()
+    app.run(host='0.0.0.0', port=port, threaded=True)
+    #app.run()
