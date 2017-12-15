f981db75c4fafa102623e23086fa886e697129c0
yamagenii <bass.g.l42@gmail.com>
2017-12-15 19:07:23 +0900

    [fix]443

--- app.py
+++ app.py
@@ -301,4 +301,5 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=443, threaded=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, threaded=True)
